b7b3bfb42a6f8a6b0cd569916130aa82e500b9d4
WQ <wq990217@gmail.com>
2024-05-20 17:19:54 +0800

    feat:unit test and icon component

--- react-unit-test/src/App.test.tsx
+++ react-unit-test/src/App.test.tsx
@@ -1,9 +1,55 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import {
+  render,
+  screen,
+  fireEvent,
+  waitFor,
+  renderHook,
+} from "@testing-library/react";
+import App from "./App";
+import Toggle from "./Toggle";
+import { act } from "react";
+import useCounter from "./useCounter";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("toggle", async () => {
+  const { container } = render(<Toggle />);
+
+  expect(container.querySelector("p")?.textContent).toBe("close");
+
+  act(() => {
+    fireEvent.click(container.querySelector("button")!);
+  });
+
+  // expect(container.querySelector("p")?.textContent).toBe("open");
+  await waitFor(
+    () => expect(container.querySelector("p")?.textContent).toBe("open"),
+    {
+      timeout: 3000,
+    }
+  );
 });
+
+test("useCounter", () => {
+  const hook = renderHook(() => useCounter(0));
+
+  const [count, increment, decrement] = hook.result.current;
+
+  act(() => {
+    increment(2);
+  });
+
+  expect(hook.result.current[0]).toBe(2);
+
+  act(() => {
+    decrement(1);
+  });
+
+  expect(hook.result.current[0]).toBe(1);
+
+  hook.unmount();
+});
+
+// test("renders learn react link", () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
